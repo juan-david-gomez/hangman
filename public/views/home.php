f286ed4281755362e5a97600a88ffcc2ec4b105d
Juan David Gomez <judagoes@gmail.com>
2016-09-10 12:15:42 -0300

    Readme y limpiar html sin usar

--- public/views/home.php
+++ public/views/home.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8" />
     <title>Hangman</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> <!-- load bootstrap via cdn -->
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css">  -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"> <!-- load fontawesome -->
+    <!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
     <link rel="stylesheet" href="css/app.css"> <!-- load fontawesome -->
 </head>
 <body ng-controller="main">
@@ -66,29 +68,12 @@
 	</div>
  </div>
 
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body">
-        
-      </div>
-      <div class="modal-footer">
-        <button type="button" ng-click="restart()" class="btn btn-primary">Reiniciar</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <!--  <script src="js/jquery.min.js"></script> -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!--<script src="js/bootstrap.min.js"></script> -->
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+    <!-- <script src="js/angular.min.js"></script> -->
     <script src="js/app.js"></script>
 </body>
 </html>
--- readme.md
+++ readme.md
@@ -1,15 +1,15 @@
 # Hangman
 
-## Configuración
+#Configuración
 * Primero se debe crear una Base de datos llamada hangman
 * copiar y renombrar el archivo .env.example a .env
 * Posteriormente en el archivo .env cambiar el valor de la variable APP_URL por el de la url del servidor
 * Despues cambiar el valor de las variables DB_USERNAME y DB_PASSWORD por usuario y contraseña de la base de datos
-* Cambiar en el archivo /public/js/app.js la constante APIURLpor la url del servidor 
+* Cambiar en el archivo /public/js/app.js la constante APIURL por la url del servidor 
 
 # Instalación
-* Estando en la raiz del proyecto ejecutar php artisan migrate --seed
 * Ejecutar composer install
-* Dar permisos de escritura recursivamente al usuario de apache a las carpetas
-* storage
-* boostrap/cache
+* Estando en la raiz del proyecto ejecutar php artisan migrate --seed
+* Dar permisos de escritura recursivamente al usuario de apache a las carpetas : storage - boostrap/cache
+
+Juan David Gómez
